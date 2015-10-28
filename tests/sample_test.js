64bbfe3066d342b1f476bc4abe2951379e5a41a3
rturak <reena@lacework.net>
2015-10-28 10:51:38 -0700

    Update sample_test.js

--- tests/sample_test.js
+++ tests/sample_test.js
@@ -6,4 +6,18 @@ module.exports = {
       .assert.containsText('body', 'Hello World')
       .end();
   }
+  "test sample1" : function (browser) {
+    browser
+      .url('http://www.google.com')
+      .waitForElementVisible('body', 1000)
+      .assert.title('Google')
+      .assert.visible('input[type=text]')
+      .setValue('input[type=text]', 'rembrandt van rijn')
+      .waitForElementVisible('button[name=btnG]', 1000)
+      .click('button[name=btnG]')
+      .pause(1000)
+      .assert.containsText('ol#rso li:first-child',
+        'Rembrandt - Wikipedia')
+      .end();
+  }
 };
