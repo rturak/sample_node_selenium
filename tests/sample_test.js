2d5e15078aadd2d593a8d8714e0f1c4d6e32f5f9
rturak <reena@lacework.net>
2015-10-28 11:16:09 -0700

    Update sample_test.js

--- tests/sample_test.js
+++ tests/sample_test.js
@@ -6,8 +6,8 @@ module.exports = {
       .assert.containsText('body', 'Hello World')
       .end();
   }
-  "test sample1" : function (browser) {
-    browser
+  "test sample1" : function (google) {
+    google
       .url('http://www.google.com')
       .waitForElementVisible('body', 1000)
       .assert.title('Google')
